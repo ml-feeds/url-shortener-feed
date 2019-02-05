d347c33aefe41840dbace2da4bcab46ed713a8ff
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-05 18:50:50 -0500

    Remove unused REPO_URL param

--- shortener/config.py
+++ shortener/config.py
@@ -15,7 +15,6 @@ CACHE_SIZE = 128
 CACHE_TTL = datetime.timedelta(minutes=58).total_seconds()
 ON_SERVERLESS = bool(os.getenv('GCLOUD_PROJECT'))
 PACKAGE_NAME = Path(__file__).parent.stem
-REPO_URL = 'https://github.com/ml-feeds/url-shortener-feed'
 SAMPLE_FEED_URL = 'https://us-east1-ml-feeds.cloudfunctions.net/kdnuggets'
 
 LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-logging
