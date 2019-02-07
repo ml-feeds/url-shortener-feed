80a5b0b419b24363d3caa9f5e2d4a1d0c3c658b3
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-06 20:06:44 -0500

    Implemented Feed class

--- scripts/feed.py
+++ scripts/feed.py
@@ -5,15 +5,17 @@ from shortener.feed import Feed
 
 log = logging.getLogger(__name__)
 
+URL = 'https://us-east1-ml-feeds.cloudfunctions.net/kdnuggets'
+
 if __name__ == '__main__':
     feed = Feed()
     try:
-        output = feed.feed('https://us-east1-ml-feeds.cloudfunctions.net/pwc/trending')
+        output = feed.feed(URL)
         print(output.decode())
 
-        # log.info('Testing cachetools cache.')
-        # assert feed.feed() == output
-        # log.info('Tested cachetools cache.')
+        log.info('Testing cachetools cache.')
+        assert feed.feed(URL) == output
+        log.info('Tested cachetools cache.')
     except Exception:
         time.sleep(.01)  # Delay for logs to flush.
         raise
---
+++ shortener/feed.py
@@ -0,0 +1,69 @@
+from functools import lru_cache
+import logging
+import urllib.error
+from urllib.request import Request, urlopen
+from xml.etree import ElementTree
+
+from bitlyshortener import Shortener as BitlyShortener
+from cachetools.func import ttl_cache
+
+from . import config
+from .util.humanize import humanize_len
+
+config.configure_logging()
+
+log = logging.getLogger(__name__)
+
+# TODO: Check for safety of URL.
+# Refer to: https://stackoverflow.com/questions/25033741/ and https://stackoverflow.com/questions/12083578/
+
+
+class FeedError(Exception):
+    def __init__(self, msg: str, code: int):
+        # msg = f'[{code}] {msg}'.rstrip()
+        self.code = code
+        log.error(msg)
+        super().__init__(msg)
+
+
+class Feed:
+    def __init__(self):
+        self._shorten_urls = BitlyShortener(tokens=config.BITLY_TOKENS,
+                                            max_cache_size=config.BITLY_SHORTENER_CACHE_SIZE).shorten_urls_to_dict
+        self._is_debug_logged = log.isEnabledFor(logging.DEBUG)
+
+    @lru_cache(maxsize=config.LRU_CACHE_SIZE)
+    def _output(self, text: str) -> bytes:
+        try:
+            xml = ElementTree.fromstring(text)
+        except ElementTree.ParseError as exception:
+            raise FeedError(f'Unable to parse URL content as XML: {exception}', 422)
+
+        long_urls = (elem.text for elem in xml.findall('./channel/item/link'))
+        url_map = self._shorten_urls(long_urls)
+        is_debug_logged = self._is_debug_logged
+        channel = next(xml.iter('channel'))
+
+        for item in list(channel.iter('item')):  # https://stackoverflow.com/a/19419905/
+            long_url = item.find('link').text
+            short_url = url_map[long_url]
+            item.find('link').text = short_url
+            if is_debug_logged:
+                log.debug('Replaced %s with %s.', long_url, short_url)
+
+        log.info('Output feed has %s items.', len(xml.findall('./channel/item')))
+        text_: bytes = ElementTree.tostring(xml)
+        return text_
+
+    @ttl_cache(maxsize=config.TTL_CACHE_SIZE, ttl=config.TTL_CACHE_TTL)
+    def feed(self, url: str) -> bytes:
+        log.debug('Reading input feed having URL %s', url)
+        request = Request(url, headers={'User-Agent': config.USER_AGENT})
+        try:
+            text = urlopen(request, timeout=config.URL_TIMEOUT).read()
+        except urllib.error.URLError as exception:
+            raise FeedError(f'Unable to read URL: {exception}', 404)
+        log.info('Input feed has size %s.', humanize_len(text))
+        text = self._output(text)
+        log.info('Output feed has size %s.', humanize_len(text))
+        return text
