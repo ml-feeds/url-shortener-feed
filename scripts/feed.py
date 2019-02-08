c3864a932a625273daa5ef12d18e23b20390329c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-08 16:39:13 -0500

    Add a debug log message

--- scripts/feed.py
+++ scripts/feed.py
@@ -5,17 +5,18 @@ from shortener.feed import Feed
 
 log = logging.getLogger(__name__)
 
-URL = 'https://feeds.feedburner.com/blogspot/gJZg'
-# URL = 'https://us-east1-ml-feeds.cloudfunctions.net/nvidia-research-ml'
+ATOM_URL = 'https://feeds.feedburner.com/blogspot/gJZg'
+RSS_URL = 'https://us-east1-ml-feeds.cloudfunctions.net/nvidia-research-ml'
 
 if __name__ == '__main__':
     feed = Feed()
+    url = ATOM_URL
     try:
-        output = feed.feed(URL)
+        output = feed.feed(url)
         print(output.decode())
 
         log.info('Testing cachetools cache.')
-        assert feed.feed(URL) == output
+        assert feed.feed(url) == output
         log.info('Tested cachetools cache.')
     except Exception:
         time.sleep(.01)  # Delay for logs to flush.
