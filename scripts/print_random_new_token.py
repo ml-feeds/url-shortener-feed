e766b0191670a8132bb8e14c10a4eaa567a3f9ce
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-09 14:17:36 -0500

    Use base32

--- scripts/print_random_new_token.py
+++ scripts/print_random_new_token.py
@@ -1,6 +1,13 @@
+import base64
 import secrets
 
-NUM_BYTES = 8
+NUM_BYTES = 10  # Must evidently be a multiple of 5 so as to avoid equal sign in b32 result.
+NUM_TOKENS = 5
 
-token = secrets.token_hex(NUM_BYTES)
-print(f'Random new token of {NUM_BYTES} bytes having length {len(token)} is {token}.')
+print('New tokens:')
+for _ in range(NUM_TOKENS):
+    token = secrets.token_bytes(NUM_BYTES)
+    token = base64.b32encode(token).decode().lower()
+    assert '=' not in token
+    assert len(token) == 16
+    print(token)
